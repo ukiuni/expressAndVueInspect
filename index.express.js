57eaff3a5902aae341f1a78b999435257e5cd1fa
ukiuni <ukiuni@gmail.com>
2019-08-26 17:18:43 +0900

    Sequelized

--- index.express.js
+++ index.express.js
@@ -1,10 +1,39 @@
 const express = require("express");
+const bodyParser = require('body-parser');//ここ
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('sample', '', '', { dialect: 'sqlite', storage: './sample.db' });
+
+var Talk = sequelize.define('Talk', {
+    message: Sequelize.STRING
+});
+
 const app = express()
+app.use(bodyParser.json())//koko
 app.use(express.static('public'));
 app.get("/api/account", (req, res) => {
-    res.json({"name":"My Name is hoge"});
+    res.json({ "name": "My Name is hoge" });
 })
+var serverTalks = ["サーバのトーク"]
+app.get("/api/talks", async (req, res) => {
+    //res.json(serverTalks);
+    res.json(await Talk.findAll());
+
+})
+app.post("/api/talks", async (req, res) => {
+    var talk = req.body.talk;
+    //serverTalks.push(talk);
+    var talkObject = Talk.build({ message: talk })
+    await talkObject.save();
+    res.send();
+})
+
 app.get("/api/work", (req, res) => {
     res.send("gege");
 })
-app.listen(3004); 
+sequelize
+    .sync({ force: false })
+    .then(function () {
+        app.listen(3004);
+    }).catch((e) => {
+        console.log(e);
+    });
